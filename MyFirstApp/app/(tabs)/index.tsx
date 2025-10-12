1beb9295a49670544067640c5d5af389d6820714
SofiaPutlyuk <sofiaputlyuk@gmail.com>
2025-10-12 08:23:50 +0300

    first app

---
+++ MyFirstApp/app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World 👋 </Text>
+      <Text style={styles.text}>How are you ?</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+});
